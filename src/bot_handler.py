cb8b4ce91b4658ce9f19da7b9be0be65d2af261a
github-workflow <>
2021-03-11 21:25:02 +0000

    Fixup: Format Python code with Black

--- src/bot_handler.py
+++ src/bot_handler.py
@@ -94,11 +94,13 @@ def verify_signature(event):
     raw_body = event.get("rawBody")
     auth_signature = event["params"]["header"].get("x-signature-ed25519")
     auth_timestamp = event["params"]["header"].get("x-signature-timestamp")
-    
+
     verify_key = VerifyKey(bytes.fromhex(args.discord_public_key))
-    
+
     message = f"{auth_timestamp}{raw_body}".encode()
-    verify_key.verify(message, bytes.fromhex(auth_signature))  # raises an error if unequal
+    verify_key.verify(
+        message, bytes.fromhex(auth_signature)
+    )  # raises an error if unequal
 
 
 def ping_pong(body):
